02e4eeb7ca871f8c0c26ee45e4491f24555722d7
Benjamin Marwell <bmarwell@gmail.com>
2020-04-20 18:22:54 +0200

    [#5] foot"inch fix, can now recognize "a foot".

--- conversion/src/main/java/io/github/bmhm/twitter/metricbot/conversion/converters/FootConverter.java
+++ conversion/src/main/java/io/github/bmhm/twitter/metricbot/conversion/converters/FootConverter.java
@@ -25,10 +25,17 @@ public class FootConverter implements UsUnitConverter {
 
   private static final Logger LOG = LoggerFactory.getLogger(FootConverter.class);
 
-  /** 1st matcher is quotation mark to find numbers. */
+  /** 1st matcher is single quotation mark to find numbers. */
   private static final Pattern FOOT_OR_FEET =
-      Pattern.compile("\\b[0-9]{0,2}\"([0-9]{0,2}|\\b)", Pattern.MULTILINE | Pattern.CASE_INSENSITIVE);
+      Pattern.compile("\\b[0-9]{0,2}'(([0-9]{0,2}(\\.[0-9]{0,2})?)(\")?|\\b)", Pattern.MULTILINE | Pattern.CASE_INSENSITIVE);
+  /**
+   * 2nd matcher is using foot|feet|ft find numbers.
+   */
+  private static final Pattern FOOT_OR_FEET_TEXT =
+      Pattern.compile("\\b([0-9]{0,2}|a)\\s*(foot|feet|ft)\\s*(([0-9]{0,2}(\\.[0-9]{0,2})?)(\")?|\\b)",
+          Pattern.MULTILINE | Pattern.CASE_INSENSITIVE);
 
+  private static final NumberFormat defaultNumberFormat = createNumberFormat();
 
   private static final String UNIT_FEET = "ft";
   private static final String UNIT_CENTIMETERS = "cm";
@@ -43,50 +50,87 @@ public class FootConverter implements UsUnitConverter {
 
   @Override
   public boolean matches(final String text) {
-    return FOOT_OR_FEET.matcher(text).find();
+    return FOOT_OR_FEET.matcher(text).find() || FOOT_OR_FEET_TEXT.matcher(text).find();
   }
 
   @Override
   public Collection<UnitConversion> getConvertedUnits(final String text) {
     final LinkedHashSet<UnitConversion> outputUnits = new LinkedHashSet<>();
-    final Matcher matcherMiles = FOOT_OR_FEET.matcher(text);
+    final Matcher matcherQuotationMark = FOOT_OR_FEET.matcher(text);
 
-    while (matcherMiles.find()) {
-      final String group = matcherMiles.group(0);
+    while (matcherQuotationMark.find()) {
+      final String group = matcherQuotationMark.group(0);
       footInchToCm(group).ifPresent(outputUnits::add);
+    }
+
+    final Matcher matcherTextFt = FOOT_OR_FEET_TEXT.matcher(text);
+    while (matcherTextFt.find()) {
+      final String feet = matcherTextFt.group(1);
+      final String inches = matcherTextFt.group(4);
+
+      if (feet.isEmpty()) {
+        continue;
+      }
+
+      footInchTextToCm(feet, inches).ifPresent(outputUnits::add);
     }
 
     return outputUnits;
   }
 
-  private Optional<UnitConversion> footInchToCm(final String group) {
-    final String[] footAndInches = group.split("\"");
+  private Optional<UnitConversion> footInchTextToCm(final String feet, final String inches) {
+    final double feetDecimal = parseFeet(feet);
+    final double inchesDecimal = parseInches(inches);
 
-    if (footAndInches.length > 2) {
-      LOG.info("Can only split foot and inches.");
-      return Optional.empty();
-    }
+    final double footDecimal = feetDecimal + (inchesDecimal / INCHES_PER_FOOT);
 
-    final double feet = parseFeet(footAndInches);
-    final double inches = parseInches(footAndInches);
-    final double footDecimal = feet + (inches / INCHES_PER_FOOT);
-
-    final NumberFormat df = DecimalFormat.getNumberInstance(Locale.US);
-    df.setMinimumFractionDigits(0);
-    df.setMaximumFractionDigits(2);
-    df.setRoundingMode(RoundingMode.HALF_UP);
-
-    final String inputFeetDecimal = df.format(footDecimal);
+    final String inputFeetDecimal = defaultNumberFormat.format(footDecimal);
 
     final double centimetres = footDecimal * CENTIMETERS_PER_FOOT;
 
     final ImmutableUnitConversion unitConversion = ImmutableUnitConversion.builder()
         .inputAmount(inputFeetDecimal)
         .inputUnit(UNIT_FEET)
-        .metricAmount(df.format(centimetres))
+        .metricAmount(defaultNumberFormat.format(centimetres))
+        .metricUnit(UNIT_CENTIMETERS)
+        .build();
+
+    return Optional.of(unitConversion);
+  }
+
+  private Optional<UnitConversion> footInchToCm(final String group) {
+    final String[] footAndInches = group.split("\'");
+
+    if (footAndInches.length > 2) {
+      LOG.info("Can only split foot and inches.");
+      return Optional.empty();
+    }
+
+    final double feet = parseFeet(footAndInches[0]);
+    final double inches = parseInches(footAndInches);
+    final double footDecimal = feet + (inches / INCHES_PER_FOOT);
+
+    final String inputFeetDecimal = defaultNumberFormat.format(footDecimal);
+
+    final double centimetres = footDecimal * CENTIMETERS_PER_FOOT;
+
+    final ImmutableUnitConversion unitConversion = ImmutableUnitConversion.builder()
+        .inputAmount(inputFeetDecimal)
+        .inputUnit(UNIT_FEET)
+        .metricAmount(defaultNumberFormat.format(centimetres))
         .metricUnit(UNIT_CENTIMETERS)
         .build();
     return Optional.of(unitConversion);
+  }
+
+  private static NumberFormat createNumberFormat() {
+    final NumberFormat df = DecimalFormat.getNumberInstance(Locale.US);
+
+    df.setMinimumFractionDigits(0);
+    df.setMaximumFractionDigits(2);
+    df.setRoundingMode(RoundingMode.HALF_UP);
+
+    return df;
   }
 
   private double parseInches(final String[] footAndInches) {
@@ -94,10 +138,32 @@ public class FootConverter implements UsUnitConverter {
       return 0.0d;
     }
 
-    return Double.parseDouble(footAndInches[1]);
+    return parseInches(footAndInches[1]);
   }
 
-  private double parseFeet(final String[] footAndInches) {
-    return Double.parseDouble(footAndInches[0]);
+  private double parseInches(final /* nullable */ String footAndInches) {
+    if (footAndInches == null) {
+      return 0.0d;
+    }
+
+    final String inches = footAndInches.split("\"", 2)[0];
+
+    if (inches.isEmpty()) {
+      return 0.0d;
+    }
+
+    return Double.parseDouble(inches);
+  }
+
+  private double parseFeet(final String footAndInches) {
+    if (footAndInches.isEmpty()) {
+      return 0.0d;
+    }
+
+    if (footAndInches.toLowerCase(Locale.ENGLISH).contains("a")) {
+      return 1.0d;
+    }
+
+    return Double.parseDouble(footAndInches);
   }
 }
--- conversion/src/test/java/io/github/bmhm/twitter/metricbot/conversion/GlobalConversionTest.java
+++ conversion/src/test/java/io/github/bmhm/twitter/metricbot/conversion/GlobalConversionTest.java
@@ -42,11 +42,17 @@ public class GlobalConversionTest {
         Arguments.of(
             "1 cup oats\n2 bananas mashed\n2 tbsp peanut butter\n1/4 tsp vanilla extract",
             List.of("1cup=236.5mL", "2tbsp=29.6mL", "0.25tsp=1.23mL")),
-        Arguments.of("5\"9", List.of("5.75ft=175.26cm")),
-        Arguments.of("he is 5\"9 or 5\"10 or 6\" tall.", List.of("5.75ft=175.26cm", "5.83ft=177.8cm", "6ft=182.88cm")),
+        Arguments.of("5\'9", List.of("5.75ft=175.26cm")),
+        Arguments.of("he is 5\'9 or 5\'10 or 6\' tall.", List.of("5.75ft=175.26cm", "5.83ft=177.8cm", "6ft=182.88cm")),
+        Arguments.of("he is 5\'9.5\" or 5\'10\" or 6\'.5\" tall.", List.of("5.79ft=176.53cm", "5.83ft=177.8cm", "6.04ft=184.15cm")),
+        Arguments.of("he is 6 foot 2 tall.", List.of("6.17ft=187.96cm")),
+        Arguments.of("he is 6 foot 2.5 tall.", List.of("6.21ft=189.23cm")),
+        Arguments.of("he is 6 foot 2.5\" tall.", List.of("6.21ft=189.23cm")),
+        Arguments.of("he is 6 foot .5\" tall.", List.of("6.04ft=184.15cm")),
+        Arguments.of("he is 6 foot 2.5 tall.", List.of("6.21ft=189.23cm")),
         Arguments.of("2 miles", List.of("2.0mi=3.2km")),
-        Arguments.of("4 feet or 16 feet", List.of("4ft=1.22m", "16ft=4.88m")),
-        Arguments.of("12 inches in a foot", List.of("12in=304.8mm"))
+        Arguments.of("4 feet or 16 feet", List.of("4ft=121.92cm", "16ft=487.68cm")),
+        Arguments.of("12 inches in a foot", List.of("1ft=30.48cm", "12in=304.8mm"))
 
     );
   }
